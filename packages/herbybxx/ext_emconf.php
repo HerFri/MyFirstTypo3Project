a715fe1711e82309b04e69495041ec9abc920f75
herbe <herbe@DESKTOP-DFLSCTP>
2024-07-18 00:57:52 +0200

    first commit

---
+++ packages/herbybxx/ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'herbybxx',
+    'description' => 'Sitepackage for TYPO3 12',
+    'category' => 'templates',
+    'author' => 'Herbert Frichert',
+    'author_email' => 'herbert.frichert@outlook.de',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
